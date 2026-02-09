fe849fab09cf476ea61ca0457440191943a023ac
patersonseanmichael-The13Horror <patersonseanmichael@gmail.com>
2026-02-09 22:53:44 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey:apiKey: "REVOKED",
+  apiKey: apiKey: "REVOKED",
   authDomain: "golden-vault-47937.firebaseapp.com",
   projectId: "golden-vault-47937",
   appId: "1:425930410009:web:31d25ad03b6d2471494288"
