358df020cab1b5c1215e3cc6604ff606660f2ba3
Michael. <patersonseanmichael@gmail.com>
2026-02-10 19:49:30 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,17 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: ""
+  apiKey: "AIzaSyAAVpPr6d7kCd3ruqs44SCT5p5tig37_QU",
+  authDomain: "golden-vault-47937.firebaseapp.com",
+  projectId: "golden-vault-47937",
+  storageBucket: "golden-vault-47937.firebasestorage.app",
+  messagingSenderId: "425930410009",
+  appId: "1:425930410009:web:31d25ad03b6d2471494288",
+  measurementId: "G-JV0QK072HR"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
