23fc9c04a0d9d31f95857cf0765f3c2c40c0ba88
Michael. <patersonseanmichael@gmail.com>
2026-02-10 13:19:39 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,10 +3,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "REVOKED",
-  authDomain: "golden-vault-47937.firebaseapp.com",
-  projectId: "golden-vault-47937",
-  appId: "1:425930410009:web:31d25ad03b6d2471494288"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
