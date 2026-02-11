99e77130d870b229b8738756b8563317f09423e3
Michael. <patersonseanmichael@gmail.com>
2026-02-11 20:01:38 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAVpPr6d7kCd3ruqs44SCT5p5tig37_QU",
-  authDomain: "golden-vault-47937.firebaseapp.com",
-  projectId: "golden-vault-47937",
-  storageBucket: "golden-vault-47937.firebasestorage.app",
-  messagingSenderId: "425930410009",
-  appId: "1:425930410009:web:31d25ad03b6d2471494288",
-  measurementId: "G-JV0QK072HR"
+  apiKey: "AIzaSyA4bJmDnapnEFlGggYVIoq2lS3QwHHz4hE",
+  authDomain: "grand-golden-vault.firebaseapp.com",
+  projectId: "grand-golden-vault",
+  storageBucket: "grand-golden-vault.firebasestorage.app",
+  messagingSenderId: "969306014666",
+  appId: "1:969306014666:web:228839a8b7f36e9b808533"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
