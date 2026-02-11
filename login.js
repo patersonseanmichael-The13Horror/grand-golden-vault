d412cfac5cc8e29b1a8ab8c4161bddee0e7e648e
Michael. <patersonseanmichael@gmail.com>
2026-02-11 19:59:18 +1000

    Update login.js

--- login.js
+++ login.js
@@ -1,23 +1,28 @@
-// login.js
-import { auth } from "./firebase.js";
-import {
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
-const form = document.getElementById("loginForm");
-const msg = document.getElementById("loginMessage");
+const firebaseConfig = {
+  apiKey: "AIzaSyA4bJmDnapnEFlGggYVIoq2lS3QwHHz4hE",
+  authDomain: "grand-golden-vault.firebaseapp.com",
+  projectId: "grand-golden-vault",
+  storageBucket: "grand-golden-vault.firebasestorage.app",
+  messagingSenderId: "969306014666",
+  appId: "1:969306014666:web:228839a8b7f36e9b808533"
+};
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-  const phone = document.getElementById("loginPhone").value;
-  const password = document.getElementById("loginPassword").value;
-  const email = `${phone}@goldenvault.com`;
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    window.location.href = "members.html";
-  } catch {
-    msg.textContent = "Invalid vault credentials";
-  }
-});
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "members.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
