aca4d954f214d59a84417a008a6dc473b908cf65
Sean Paterson <sean@example.com>
2026-02-21 03:08:50 +1000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
-import nextVitals from 'eslint-config-next/core-web-vitals';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [...nextVitals];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default [...compat.extends("next/core-web-vitals")];
